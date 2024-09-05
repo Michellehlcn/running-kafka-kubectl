2e902ea48c985bf263feea61468103c422bb6cd5
michellehlcn <michellehlcn.au@gmail.com>
2024-09-05 12:13:00 +1000

    add

--- infra/cdk/lib/cdk-stack.ts
+++ infra/cdk/lib/cdk-stack.ts
@@ -53,6 +53,60 @@ export class CdkStack extends cdk.Stack {
       }
     )
 
+
+    const readerInstance = cdk.aws_rds.ClusterInstance.provisioned('reader-instance', {
+      instanceType: cdk.aws_ec2.InstanceType.of(cdk.aws_ec2.InstanceClass.BURSTABLE4_GRAVITON, cdk.aws_ec2.InstanceSize.MEDIUM),
+      instanceIdentifier: 'reader-instance',
+    })
+
+    // Create aurora postgreSQL compatible cluster 
+    const cluster = new cdk.aws_rds.DatabaseCluster(this, 'AuroraDatabaseCluster', {
+      engine: cdk.aws_rds.DatabaseClusterEngine.auroraPostgres({
+        version: EngineVersion,
+      }),
+      vpc: vpc,
+      vpcSubnets: { subnetType: SubnetType.PUBLIC },
+      clusterIdentifier: 'sample',
+      writer: writerInstance,
+      readers: [readerInstance],
+      defaultDatabaseName: 'sample',
+      credentials: cdk.aws_rds.Credentials.fromGeneratedSecret('admin', { secretName: 'sample'}),
+      securityGroups: [rdsSecurityGroup],
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    })
+
+    // Create a VPC endpoint for secretManager
+    const secretManagerVpcEndpoint = new cdk.aws_ec2.InterfaceVpcEndpoint(this, 'SecretManagerVpcEndpoint', {
+      service: cdk.aws_ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+      vpc: vpc,
+      subnets: { subnetType: SubnetType.PUBLIC },
+      securityGroups: [serverlessSecurityGroup],
+    })
+
+    // update the serverless application security group to allow outbound traffic
+    secretManagerVpcEndpoint.connections.securityGroups.forEach(sg=>{
+      serverlessSecurityGroup.addEgressRule(
+        cdk.aws_ec2.Peer.securityGroupId(sg.securityGroupId), 
+        cdk.aws_ec2.Port.tcp(443),
+        "Allow HTTPS outbound traffic from serverless application"
+    )
+  })
+  // configure the VPC endpoint's Security Groups to allow inbound traffic from the serverless application
+  secretManagerVpcEndpoint.connections.securityGroups.forEach(sg=>{
+    sg.addIngressRule(
+      serverlessSecurityGroup,
+      cdk.aws_ec2.Port.tcp(443),
+      "Allow HTTPS inbound traffic to Secret Manager VPC endpoint"
+  )
+  })
+
+  // Allow the serverless application to connect to the database
+  cluster.connections.allowFrom(serverlessSecurityGroup, cdk.aws_ec2.Port.tcp(cluster.clusterEndpoint.port))
+
+  // Outputs
+  new cdk.CfnOutput(this, 'ClusterARN', { value: cluster.clusterEndpoint.hostname })
+  new cdk.CfnOutput(this, 'ClusterSecretARN', { value: cluster.secret!.secretArn })
+  // OpenSearch Service Domain
     // The code that defines your stack goes here
 
     // example resource
